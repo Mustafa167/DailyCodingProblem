0633e53261c1c337988cc77a7a01b65672acbf7b
Mustafa167 <attariamustafa@gmail.com>
2020-03-15 17:33:35 +0530

    Max number of buildings
    Updated code to count maximum number of buildings that can see the sunrise in east

--- Python/285_SunsetBuilding.py
+++ Python/285_SunsetBuilding.py
@@ -1,1 +1,10 @@
-print ("Hello World")
+def getNumberOfBuilding(arr):
+  count = 1
+  length = len(arr)
+  for i in range(1,length):
+    if(arr[i - 1] > arr[i]):
+      count += 1
+  return count
+
+arr = [3, 7, 8, 3, 6, 1]
+print(getNumberOfBuilding(arr))
