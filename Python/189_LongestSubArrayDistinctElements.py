5c139a36605914583088fabd2d1351c482e34da4
Mustafa Saifuddin Attaria <attariamustafa@gmail.com>
2020-03-29 20:20:54 +0530

    Update 189_LongestSubArrayDistinctElements.py
    Solution using set() in python

--- Python/189_LongestSubArrayDistinctElements.py
+++ Python/189_LongestSubArrayDistinctElements.py
@@ -5,21 +5,19 @@ Given an array of elements, return the length of the longest subarray where all 
 For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 '''
 
-numArray = [5, 1, 3, 5, 2, 3, 4, 1, 0]
-hashArray = [0] * len(numArray)
+numArray =[5, 1, 3, 5, 2, 3, 4, 1]
+hashArray = set()
 maxCount = 1
 count = 1
 
 for num in numArray:
-  if hashArray[num] != 1:
-    hashArray[num] = 1
-    count += 1
-    if count > maxCount:
-      maxCount = count
-  else:
-    hashArray = [0] * len(numArray)
+  if num in hashArray:
+    hashArray.clear();
     count = 1
+  else:
+    hashArray.add(num)
+    if maxCount < count:
+      maxCount = count
+    count += 1
 
 print(maxCount)
-
-
